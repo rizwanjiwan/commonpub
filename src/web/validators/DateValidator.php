96d6dfaee2fc2dde65919373095949b791993194
Rizwan Jiwan <33099935+rizwanjiwan@users.noreply.github.com>
2021-02-23 07:42:02 -0500

    Allow date validator code to be used on normal strings

--- src/web/validators/DateValidator.php
+++ src/web/validators/DateValidator.php
@@ -17,27 +17,39 @@ class DateValidator implements Validator
 {
 
 
-	/**
-	 * Validate a field against this Validators criteria
-	 * @param $field AbstractField to validate
-	 * @param $fields NameableContainer of AbstractField for the other fields values that might be needed in validation
-	 * @throws InvalidValueException if not valid
-	 */
-	public function validate(AbstractField $field, NameableContainer $fields)
-	{
-		$value=$field->getValue();
-		if(strlen($value)===0)
-			return;//nothing to check
-		try
-		{
-			if(is_int($value))
-				new DateTime(strtotime('Y-m-d H:i',$value));
-			else
-				new DateTime($value);//all is good
-		}
-		catch (Exception $e) //invalid format
-		{
-			throw new InvalidValueException('Invalid Date: '.$value);
-		}
-	}
+    /**
+     * Validate a field against this Validators criteria
+     * @param $field AbstractField to validate
+     * @param $fields NameableContainer of AbstractField for the other fields values that might be needed in validation
+     * @throws InvalidValueException if not valid
+     */
+    public function validate(AbstractField $field,NameableContainer $fields)
+    {
+        $value=$field->getValue();
+        if(strlen($value)===0)
+            return;//nothing to check
+        self::isValid($value);
+    }
+
+    /**
+     * Validate any old date string
+     * @param string $dateString
+     * @return DateTime valid date
+     * @throws InvalidValueException if the string can't be interpreted as a date (or is empty/null)
+     */
+    public static function isValid(string $dateString):DateTime
+    {
+        if(strlen($dateString)===0)
+            throw new InvalidValueException('Invalid Date: '.$dateString);
+        try
+        {
+            if(is_int($dateString))
+                return new DateTime(strtotime('Y-m-d H:i',$dateString));
+            return new DateTime($dateString);//all is good
+        }
+        catch (Exception) //invalid format
+        {
+            throw new InvalidValueException('Invalid Date: '.$dateString);
+        }
+    }
 }
