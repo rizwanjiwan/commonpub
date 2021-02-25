b24e7fc6fe9c5c22452a1e6ae5b42e824e83477b
Rizwan Jiwan <33099935+rizwanjiwan@users.noreply.github.com>
2021-02-24 19:29:54 -0500

    Add slack helper

---
+++ src/classes/SlackHelper.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace rizwanjiwan\common\classes;
+
+
+use Slack;
+use SlackMessage;
+
+/**
+ * Send messages via slack webhook
+ * Class SlackHelper
+ * @package rizwanjiwan\common\classes
+ */
+class SlackHelper
+{
+
+    public static function sendMessage(string $message)
+    {
+        if(Config::get(('SLACK_WEBHOOK_URL'))!==null)
+        {
+            self::sendMessageReal($message,Config::get('SLACK_WEBHOOK_URL'));
+        }
+
+    }
+
+    private static function sendMessageReal(string $message, string $webhookUrl)
+    {
+        $slackMessage=new SlackMessage(new Slack($webhookUrl));
+        $slackMessage->setText($message);
+        $slackMessage->send();
+
+    }
+}
